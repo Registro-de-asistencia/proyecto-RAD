137a02f670f38b0e831203dd580ab75e3c7e2e63
Henry <cajapasto5@gmail.com>
2020-03-19 15:01:05 -0300

    vv

---
+++ ventanasemer/js/validator.js
@@ -0,0 +1,21 @@
+var nombreout = document.querySelector('#Input_Nombre');
+var apellidoout = document.querySelector('#Input_Apellido');
+var rutout = document.querySelector('#Input_Rut');
+var areaout = document.querySelector('#Input_Area');
+var exigidasout = document.querySelector('#Input_Exijidas');
+var imagenformulario = document.querySelector('#Input_Imagen');
+
+//var formulario = document.querySelector('#form').value;
+
+
+/*name validation!*/
+nombreout.addEventListener('blur', function() {
+    name = nombreout.value;
+    if (name.null == 0 || name.length == 0) {
+        console.log(name + " nada");
+
+    } else {
+
+        console.log(name);
+    }
+});
